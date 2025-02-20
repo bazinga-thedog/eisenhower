64f8a80bd55e75d4e75e79e3540ad2f296ea50eb
bazinga-thedog <thedogbazinga@gmail.com>
2025-02-20 23:24:46 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+interface ApiResponse {
+  id: BigInteger;
+  description: string;
+}
+
+export default function App() {
+  const [data, setData] = useState<ApiResponse[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3030/api/task")
+      .then((response) => response.json())
+      .then((data: ApiResponse[]) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-2">API Data</h1>
+      <ul className="list-disc pl-5">
+        {
+        data.map((item, index) => (
+          <li key={index}>{item.id} - {item.description}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
