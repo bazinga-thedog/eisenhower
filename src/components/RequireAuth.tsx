6a5e9ad78b6dab47e1fdf85f73703db38fe289d5
bazinga-thedog <thedogbazinga@gmail.com>
2025-02-27 11:37:42 +0000

    Change requireauth from js to tsx

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,13 +1,17 @@
 import { useLocation, Navigate, Outlet } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
 
-const RequireAuth = ({ allowedPermissions }) => {
+const RequireAuth = ({
+  allowedPermissions,
+}: {
+  allowedPermissions: string[]
+}) => {
   const { accessToken, permissions } = useAuth()
   const location = useLocation()
 
   return permissions?.find(role => allowedPermissions?.includes(role)) ? (
     <Outlet />
-  ) : accessToken > 0 ? (
+  ) : accessToken ? (
     <Navigate to="/unauthorized" state={{ from: location }} replace />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
